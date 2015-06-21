f976f635ea78c52cef966a9911346143b746ba24
huba193 <huubeertt@wp.pl>
2015-06-22 00:06:07 +0200

    poprawa wyświetlania

--- src/rmiclient/RMIClient.java
+++ src/rmiclient/RMIClient.java
@@ -12,49 +12,47 @@ import rmii.RMII;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Hubert
  */
 public class RMIClient {
-    public static void main(String args[]){
+
+    public static void main(String args[]) {
         RMIClient client = new RMIClient();
         client.connectServer();
     }
-   
+
     private void connectServer() {
         try {
             Registry reg = LocateRegistry.getRegistry("192.168.0.100", 5099);
             RMII rmi = (RMII) reg.lookup("server");
             System.out.println("Connected to server");
-           
+
             String tmp = "", tmp1 = "", tmp2 = "";
             int ilosc = 3;
             Scanner odczyt = new Scanner(System.in);
             System.out.println("Podaj ilosc: ");
             tmp2 = odczyt.nextLine();
-           
-            
+
             try {
                 ilosc = Integer.parseInt(tmp2);
-                System.out.println(ilosc);
-                
             } catch (Exception e) {
                 System.out.println("Podano niepoprawne dane!!!");
-                
+
             }
-            
-            for (int i=0; i<ilosc; i++){
+            String[] tab = new String[ilosc];
+            System.out.println("Podaj wysokość rolet: ");
+            tmp1 = odczyt.nextLine();
+            for (int i = 0; i < ilosc; i++) {
                 System.out.println("Podaj szerokość: ");
                 tmp = odczyt.nextLine();
-                System.out.println("Podaj wysokość: ");  
-                tmp1 = odczyt.nextLine();
+                tab[i] = tmp;
+
             }
             String p;
-            System.out.println(rmi.obliczenia(rmi.zapisDanych(ilosc, tmp, tmp1)));
-          
-            
+            System.out.println(rmi.obliczenia(rmi.zapisDanych(ilosc, tab, tmp1)));
+
         } catch (Exception e) {
             System.out.println(e);
         }
